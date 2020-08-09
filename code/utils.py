ea96c5c028deaaf026a90e435128e0e1d948ed2a
Brian Jackson <bjackson@boisestate.edu>
2020-08-09 12:37:52 -0600

    Added fill_gaps method

--- code/utils.py
+++ code/utils.py
@@ -190,13 +190,58 @@ def retrieve_pressure_data(sol, dr=None):
     LTST_and_sol = LTST_and_sol[unq]
     sol_data = sol_data[unq]
 
-    # Some of the time-series are out of order for some reason
-    srt = np.argsort(LTST_and_sol)
-    LTST = LTST[srt]
-    LTST_and_sol = LTST_and_sol[srt]
-    sol_data = sol_data[srt]
+    # Fill gaps
+    LTST, LTST_and_sol, sol_data = fill_gaps(LTST, LTST_and_sol, sol_data)
 
     return LTST, LTST_and_sol, sol_data
+
+def fill_gaps(LTST, LTST_and_sol, sol_data):
+    """ Fill gaps in the time-series """
+
+    ret_LTST = LTST
+    ret_LTST_and_sol = LTST_and_sol
+    ret_sol_data = sol_data
+
+    delta_ts = (LTST_and_sol[1:] - LTST_and_sol[0:-1])      
+    ind = delta_ts > 0.
+    mod = mode(delta_ts[ind])[0][0]
+
+    # If there are no gaps in the time-series
+    if(len(delta_ts[~np.isclose(delta_ts, mod)]) == 0):
+        return LTST, LTST_and_sol, sol_data
+    else:
+        # If there are any gaps at all, there seems only to be one gap.
+        # Fill that gap with nans.
+        gaps = np.argwhere(~np.isclose(delta_ts, mod))[0]
+
+        ret_LTST_and_sol =\
+            np.concatenate((LTST_and_sol[0:gaps[0]], 
+            np.arange(LTST_and_sol[gaps[0]] + mod,
+                LTST_and_sol[gaps[0]+1] - mod, mod), 
+            LTST_and_sol[gaps[0]+1:-1]))
+
+        ret_LTST =\
+            np.concatenate((LTST[0:gaps[0]], 
+            np.arange(LTST[gaps[0]] + mod, LTST[gaps[0]+1] - mod, mod), 
+            LTST[gaps[0] + 1:-1]))
+
+        # Create temp array to get correct shape
+        first_name = sol_data.dtype.names[0]
+        temp_array = np.concatenate((sol_data[first_name][0:gaps[0]],
+                                     np.full_like(np.arange(LTST[gaps[0]] + mod,
+                                         LTST[gaps[0]+1] - mod, mod), np.nan),
+                                     sol_data[first_name][gaps[0] + 1:-1]))
+        ret_sol_data = np.empty(temp_array.shape, dtype=sol_data.dtype)
+        for cur_name in sol_data.dtype.names:
+            ret_sol_data[cur_name] =\
+                    np.concatenate((sol_data[cur_name][0:gaps[0]],
+                        np.full_like(np.arange(LTST[gaps[0]] + mod, 
+                            LTST[gaps[0]+1] - mod, mod), np.nan),
+                        sol_data[cur_name][gaps[0] + 1:-1]))
+
+
+        return ret_LTST, ret_LTST_and_sol, ret_sol_data
+
 
 def boxcar_filter(data, boxcar_window_size):
     filt = savgol_filter(data, boxcar_window_size, 0, mode='nearest')
